49b59a57019d73fd213633fecfcb7edaea01b25d
ahmed <ametwally494@gmail.com>
2024-03-11 22:51:53 +0200

    first commit

---
+++ Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class Account
+    {
+        public double Balance { get; set; }
+        public string Name { get; set; }
+
+        public Account(double balance=0, string name="null")
+        {
+            this.Balance = balance;
+            this.Name = name;
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public bool WithDrow(double amount)
+        {
+            if (amount>Balance)
+            {
+                return false;
+            } 
+                Balance -= amount;
+                return true;     
+        }
+         public void Print()
+        {
+            Console.WriteLine($"Your Name is: {Name}");
+              Console.WriteLine($"Your Balance is: {Balance}");
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+
+            Account account = new Account(name:"Ahmed");
+            
+
+            do
+            {
+               
+                Console.WriteLine("1. Deposit");
+                Console.WriteLine("2. Withdraw");
+                Console.WriteLine("3. Print");
+                Console.WriteLine("4. Quit");
+
+                Console.Write("Please select an option:");
+                int option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        DoDeposit(account);
+                        break;
+                    case 2:
+                        DoWithdraw(account);
+                        break;
+                    case 3:
+                        DoPrint(account);
+                        break;
+                    case 4:
+                        return;
+                        Console.WriteLine("Goodbey");
+                }
+
+
+            } while (true);
+
+          
+
+           
+        }
+        public static void DoDeposit(Account account)
+        {
+            Console.WriteLine("Please enter the amount to deposit:");
+            double amount = double.Parse(Console.ReadLine());
+
+            account.Deposit(amount);
+
+            Console.WriteLine("Deposit successful!");
+        }
+        public static void DoWithdraw(Account account)
+        {
+            Console.WriteLine("Please enter the amount to withdraw:");
+            double amount = double.Parse(Console.ReadLine());
+
+            if (!account.WithDrow(amount))
+            {
+                Console.WriteLine("Insufficient funds!");
+                return;
+            }
+
+            Console.WriteLine("Withdrawal successful!");
+        }
+        public static void DoPrint(Account account)
+        {
+            account.Print();
+        }
+
+    }
+}
+
